4cd057e51beddc21519df88fb8b5fe2e895f746f
Kevin Mahlin <kmahlin@gmail.com>
2016-03-10 01:40:21 -0600

    added flatten and recombine

--- csce322homework03part01.hs
+++ csce322homework03part01.hs
@@ -14,25 +14,33 @@ main = do
 -- YOUR CODE SHOULD COME AFTER THIS POINT
 onePlayerOneRotation :: [[Char]] -> [Char] -> [[Char]]
 onePlayerOneRotation maze move
-  | move == "c"   = clockwiseRotation (maze)
-  | move == "cc"  = counterClockwiseRotation (maze)
-  | move == "180" = oneHundredEightyRotation (maze)
+  | move == "c"   = recombine cols (flatten c)
+  | move == "cc"  = cc
+  | move == "180" = recombine cols (flatten oneEighty)
   | otherwise     = ["wrong"]
-
-
-
--- move player
-movePlayer :: Char -> [Char] -> [Char]
-movePlayer player maze
-  |  playerMovable = maze -- -- swap move player
-  | otherwise = ['n','o'] -- todo change this, atm just for debugging
   where
-    playerMovable = isMovable position maze
-    position      = (head (findPlayerIndex player maze))
+    c         = clockwiseRotation (maze)
+    cc        = counterClockwiseRotation (maze)
+    oneEighty = oneHundredEightyRotation (maze)
+    cols      = length (head maze)
 
 
--- I want to say, if dash, move, if x stop
--- you would give it a player position, and as if the next space is open
+
+
+
+
+recombine :: Int -> [Char] -> [[Char]]
+recombine _ [] = []
+recombine cut maze
+  | cut > 0 = (take cut maze) : (recombine cut (drop cut maze))
+  | otherwise = []
+
+
+-- take a 2d char list and flatten it to a 1d list
+flatten :: [[Char]] -> [Char]
+flatten maze =  concat(maze)
+
+-- checks if position + 1 is a movable position for a player
 isMovable :: Int -> [Char] -> Bool
 isMovable pos maze
   | valueAtIndex (pos + 1) maze == '-' = True
@@ -40,28 +48,22 @@ isMovable pos maze
   | otherwise = False
 
 
---just moves player 1 atm
-move :: Char -> [Char] -> [Char]
-move _ [] = []
-move player (he:ta)
-  | he == player && movable  = '-' : move player ( player : tail ta )
-  | otherwise                = he : move player ta
+--move player forward
+-- TODO account for stopping at g
+movePlayer :: Char -> [Char] -> [Char]
+movePlayer _ [] = []
+movePlayer player (he:ta)
+  | he == player && movable  = '-' : movePlayer player ( player : tail ta )
+  | otherwise                = he  : movePlayer player ta
   where
     movable = isMovable 0 (he:ta)
 
--- Get all players
-getPlayers :: [Char] -> [Char] -> [Char]
-getPlayers arr [] = arr
-getPlayers arr (he:ta)
-  | he == '1' =  getPlayers ('1' :  arr) ta
-  | he == '2' =  getPlayers ('2' :  arr) ta
-  | he == '3' =  getPlayers ('3' :  arr) ta
-  | he == '4' =  getPlayers ('4' :  arr) ta
-  | otherwise =  getPlayers  arr ta
-
-swap :: Int -> Int -> [Int] -> [Int]
-swap _ _ [] = []
-swap n m (x:xs)
-  | n == x = m : (swap n m xs)
-  | m == x = n : (swap n m xs)
-  | otherwise = x : (swap n m xs)
+-- returns a list of players, lowest player is first
+getPlayersOrd :: [Char] -> [Char] -> [Char]
+getPlayersOrd arr [] = arr
+getPlayersOrd arr (he:ta)
+  | he == '1' =  getPlayersOrd ('1' :  arr) ta
+  | he == '2' =  getPlayersOrd ('2' :  arr) ta
+  | he == '3' =  getPlayersOrd ('3' :  arr) ta
+  | he == '4' =  getPlayersOrd ('4' :  arr) ta
+  | otherwise =  getPlayersOrd  arr ta
